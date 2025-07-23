19fdc9b9dd7b85258c36960ed60f202dbff84683
Alberth Brendon <alberth.brendon@gmail.com>
2025-07-22 22:45:01 -0300

    Commit atividade de Colletions

---
+++ EXERCICIOsEbac/src/Colletions_EBAC01/Colletions.java
@@ -0,0 +1,23 @@
+package Colletions_EBAC01;
+
+public class Colletions {
+	
+	//static String nome = "ALBERTH, ADS";
+	
+	//static String[] nomes = nome.split(",");
+	
+	static ListaPessoas lp = new ListaPessoas();
+	
+	public static void main(String[] args) {
+		
+		//System.out.println("NOME: " + nomes[0]);
+		//System.out.println("CURSO: " + nomes[1]);
+		
+		lp.AddNaListaPessoas();
+		lp.ExibirLista();
+		
+		System.out.println("========================================================================================================================= PROGRAMA ENCERRADO By. Alberth =====================");
+		
+	}
+
+}
